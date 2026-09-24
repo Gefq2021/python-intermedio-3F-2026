424c70bc956c389d87731b1a899fc7ec1b3217c3
Gerardo Fabian Quispe <gfq0684@gmail.com>
2026-09-24 15:06:12 -0300

    agregamos el ejercicio 5

---
+++ practica_1_conjuntos/ejercicio_5.py
@@ -0,0 +1,15 @@
+# PYTHON INTERMEDIO
+# Practica de Conjunto
+# Quispe, Gerardo Fabián
+
+# Ejercicio 5:
+# Dados un conjunto, A, escribe un programa en Python que imprima el número de elementos del conjunto.
+
+A = {3, 5, 1, 7, 9}
+B = {1, 4, 5, 6, 8, 3, 2}
+
+print("A = ", A)
+print("El conjunto A, tiene ", len(A), " elementos.")
+
+print("\nB = ", B)
+print("El conjunto B, tiene ", len(B), " elementos.")
