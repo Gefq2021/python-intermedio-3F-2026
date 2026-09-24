1e56d73e0f7356ac37f4a29c0ce2458d617df0a2
Gerardo Fabian Quispe <gfq0684@gmail.com>
2026-09-24 15:05:04 -0300

    agregamos el ejercicio 2

---
+++ practica_1_conjuntos/ejercicio_2.py
@@ -0,0 +1,15 @@
+# PYTHON INTERMEDIO
+# Practica de Conjunto
+# Quispe, Gerardo Fabián
+
+# Ejercicio 2:
+# Dados dos conjuntos, A y B, escribe un programa en Python que imprima los elementos que se encuentran en A y en B.
+
+A = {3, 5, 1, 7, 9}
+B = {1, 4, 5, 6, 8, 3, 2}
+
+print("A = ", A)
+print("B = ", B)
+
+print("\nElementos que se encuentran en A y en B:")
+print(A & B)
